2ba1f5ec6117ab0460a57be6369d471a627a6886
Jay Kalio <jerrykalio@gmail.com>
2021-05-04 00:37:09 -0500

    Write reducer code.

---
+++ core-redux-concepts.js
@@ -0,0 +1,10 @@
+// Core Redux Concepts
+// Fill in the reducer function so that it updates state immutably. The setToLate action should set the isLate property to true.
+
+const initialState = {
+   isLate: false,
+   ETA: '11:00',
+   destination: 'Mars'
+};
+
+const arrivalReducer = (state = initialState, action) => {};
