24b32c97fcc20dde96e1bb8bcefb136accee97c1
Jay Kalio <jerrykalio@gmail.com>
2021-05-10 18:25:38 -0500

    Modify todoReducer function.

--- core-redux-concepts.js
+++ core-redux-concepts.js
@@ -23,3 +23,11 @@ const arrivalReducer = (state = initialState, action) => {
 
 // Fill in the reducer function so that it updates state immutably. The addTodo action should add the action.payload value to the state array.
 
+const todoReducer = (state = [], action) => {
+  switch (action.type) {
+    case 'addTodo':
+      return [...state, action.payload];
+    default:
+      return state;
+  }
+}
